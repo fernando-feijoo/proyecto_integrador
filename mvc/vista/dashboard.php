e8af09f0a92b8af54ba10f690324147ea7dab4ef
PC ESCRITORIO <fernando95f4@gmail.com>
2023-01-26 17:11:45 -0500

    Cambios de inicio, tengo que hacer merge de mi rama.

--- mvc/vista/dashboard.php
+++ mvc/vista/dashboard.php
@@ -22,47 +22,7 @@ if (empty($_SESSION["id"])) {
 </head>
 
 <body>
-    <header>
-        <div id="header" class="container-fluid pt-1 pb-1">
-            <div class="row d-flex align-items-center mx-2">
-                <div class="col">
-                    <img class="img-fluid" src="../../img/LogoTierraFertil_305px_75px-dashboard.png" alt="logo-empresa" />
-                </div>
-                <div class="col d-flex align-items-end flex-column">
-
-                    <nav class="navbar navbar-expand">
-                        <div class="container">
-
-                            <div class="collapse navbar-collapse" id="navbarNav">
-                                <ul class="navbar-nav">
-
-                                    <li class="nav-item dropdown">
-                                        <a id="texto-usuario" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <img src="../../img/ProfileUser_32px-dashboard.png" alt="icono-usuario" />
-                                            <?php
-                                            echo $_SESSION["nombre"];
-                                            ?>
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li><a class="dropdown-item text-center" href="#" role="switch">
-                                                    <div class="form-check form-switch">
-                                                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                                                        <label class="form-check-label" for="flexSwitchCheckDefault">Modo Ocuro</label>
-                                                    </div>
-                                                </a></li>
-                                            <li><a class="dropdown-item text-center" href="./../controlador/controlador_cerrar_sesion.php"><img src="../../img/log_out_24px-dashboard.png" alt="icono-logout" style="margin-right: 12px;" />Cerrar Sesion</a></li>
-                                        </ul>
-                                    </li>
-
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-
-                </div>
-            </div>
-        </div>
-    </header>
+    <?php require "./layout/header.php"?>
 
     <main>
         <div id="escritorio-contenedor" class="container-fluid pt-2">
@@ -70,11 +30,11 @@ if (empty($_SESSION["id"])) {
                 <!-- Contenedor de menu lateral -->
                 <?php
                 if ($_SESSION["rol"] == "grupo1") {
-                    include "./Grupo 1/menu_grupo1.php";
+                    require "./Grupo 1/menu_grupo1.php";
                 } elseif ($_SESSION["rol"] == "grupo2") {
-                    include "./Grupo 2/menu_grupo2.php";
+                    require "./Grupo 2/menu_grupo2.php";
                 } elseif ($_SESSION["rol"] == "grupo3") {
-                    include "./Grupo 3/menu_grupo3.php";
+                    require "./Grupo 3/menu_grupo3.php";
                 }
                 ?>
                 <!-- /Contenedor de menu lateral -->
@@ -82,11 +42,11 @@ if (empty($_SESSION["id"])) {
                 <!-- Contenedor de escritorio -->
                 <?php
                 if ($_SESSION["rol"] == "grupo1") {
-                    include "./Grupo 1/escritorio_grupo1.php";
+                    require "./Grupo 1/escritorio_grupo1.php";
                 } elseif ($_SESSION["rol"] == "grupo2") {
-                    include "./Grupo 2/escritorio_grupo2.php";
+                    require "./Grupo 2/escritorio_grupo2.php";
                 } elseif ($_SESSION["rol"] == "grupo3") {
-                    include "./Grupo 3/escritorio_grupo3.php";
+                    require "./Grupo 3/escritorio_grupo3.php";
                 }
                 ?>
                 <!-- /Contenedor de escritorio -->
