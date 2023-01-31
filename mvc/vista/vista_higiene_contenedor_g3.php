b14c930d29eddf732a53137afe8ad36a3d6e49d5
bryan5243 <chuchei5243@gmail.com>
2023-01-31 00:18:13 -0500

    concluido_vistas

--- mvc/vista/vista_higiene_contenedor_g3.php
+++ mvc/vista/vista_higiene_contenedor_g3.php
@@ -19,49 +19,49 @@
                     <tr>
                         <td> Llegó lavado</td>
                         <td>
-                            <input class="lavado_si" type="radio" id="lavado_si" value="si"> Si
-                            <input class="lavado_no" type="radio" id="lavado_no" value="no"> No
+                            <input class="lavado_si" type="radio" id="lavado_si" name="lavado" value="si"> Si
+                            <input class="lavado_no" type="radio" id="lavado_no" name="lavado" value="no"> No
                         </td>
                         <td> Presencia de insectos en el contenedor</td>
                         <td>
-                            <input class="insectos_si" type="radio" id="insectos_si" value="si"> Si
-                            <input class="insectos_no" type="radio" id="insectos_mo" value="no"> No
+                            <input class="insectos_si" type="radio" id="insectos_si" name="insectos" value="si"> Si
+                            <input class="insectos_no" type="radio" id="insectos_mo"name="insectos"  value="no"> No
                         </td>
                     </tr>
                     <tr>
                         <td> Drenajes limpios</td>
                         <td>
-                            <input class="drenajes_si" type="radio" id="drenajes_si" value="si"> Si
-                            <input class="drenajes_no" type="radio" id="drenajes_no" value="no"> No
+                            <input class="drenajes_si" type="radio" id="drenajes_si" name="drenaje" value="si"> Si
+                            <input class="drenajes_no" type="radio" id="drenajes_no" name="drenaje" value="no"> No
                         </td>
                         <td> Llego con sello de llegada</td>
                         <td>
-                            <input class="sello_si" type="radio" id="sello_si" value="si"> Si
-                            <input class="sello_no" type="radio" id="sello_no" value="no"> No
+                            <input class="sello_si" type="radio" id="sello_si" name="sello" value="si"> Si
+                            <input class="sello_no" type="radio" id="sello_no" name="sello" value="no"> No
                         </td>
                     </tr>
                     <tr>
                         <td> Olor aceptable</td>
                         <td>
-                            <input class="olor_si" type="radio" id="olor_si" value="si"> Si
-                            <input class="olor_no" type="radio" id="olor_no" value="no"> No
+                            <input class="olor_si" type="radio" id="olor_si" name="olor" value="si"> Si
+                            <input class="olor_no" type="radio" id="olor_no" name="olor" value="no"> No
                         </td>
                         <td> Presencia de moho dentro del contenedor</td>
                         <td>
-                            <input class="moho_si" type="radio" id="moho_si" value="si"> Si 
-                            <input class="moho_no" type="radio" id="moho_no" value="no"> No
+                            <input class="moho_si" type="radio" id="moho_si"  name="moho" value="si"> Si 
+                            <input class="moho_no" type="radio" id="moho_no"  name="moho" value="no"> No
                         </td>
                     </tr>
 
                     <tr>
                         <td> Residuos químicos</td>
-                        <td><input class="quimicos_si" type="radio" id="quimicos_si"  value="si"> Si
-                            <input class="quimicos_no" type="radio" id="quimicos_no"  value="no"> No
+                        <td><input class="quimicos_si" type="radio" id="quimicos_si" name="quimicos"  value="si"> Si
+                            <input class="quimicos_no" type="radio" id="quimicos_no" name="quimicos"  value="no"> No
                         </td>
                         <td> Contenedor aceptable</td>
                         <td>
-                            <input class="contenedor_si" type="radio" id="contenedor_si"  value="si"> Si 
-                            <input class="contenedor_no" type="radio" id="contenedor_no"  value="no"> No </td>
+                            <input class="contenedor_si" type="radio" id="contenedor_si" name="contenedor"   value="si"> Si 
+                            <input class="contenedor_no" type="radio" id="contenedor_no"  name="contenedor" value="no"> No </td>
                     </tr>
                 </tbody>
             </table>
