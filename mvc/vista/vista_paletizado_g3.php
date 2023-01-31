230147d3eb9fc08075687cf166fea46d588bee91
bryan5243 <chuchei5243@gmail.com>
2023-01-31 00:14:57 -0500

    concluido_vistas

---
+++ mvc/vista/vista_paletizado_g3.php
@@ -0,0 +1,139 @@
+<div  class="row mx-auto">
+    <div class="containe ">
+        <div class="row">
+            <div class="col form-check form-check-inline   fs-5 fw-semibold">
+                CONTROL DE PALETIZADO EN CONTENEDOR
+
+            </div>
+            <div class="d-flex fw-semibold" style="margin-left: 80%;margin-top: 10px;">
+                Numero de cajas:
+                <select name="cajas">
+                    <option value="48">48</option>
+                    <option value="53">53</option>
+
+                </select>
+
+
+            </div>
+            <div style="margin-left: 50px;">
+                <table class="table-bordered  " style="margin-top: 10px;border-color: black;">
+
+                    <thead style="background-color: gray;">
+
+                        <th style="width: 20px;">1</th>
+                        <th><input class="text-uppercase" type="text" name="caja_1"></th>
+
+                        <th>6</th>
+                        <th><input class="text-uppercase" type="text" name="caja_6"></th>
+
+                        <th>11</th>
+                        <th><input class="text-uppercase" type="text" name="caja_11"></th>
+
+                        <th>16</th>
+                        <th><input class="text-uppercase" type="text" name="caja_16"></th>
+
+                    </thead>
+
+                    <tbody style="background-color: white;">
+                        <tr>
+                            <td class="fw-bold" style="background-color: gray;"> 2</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_2">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 7</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_7">
+
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 12</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_12">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 17</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_17">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="fw-bold" style="background-color: gray;"> 3</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_3">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 8</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_8">
+
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 13</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_13">
+                                
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 18</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_18">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="fw-bold" style="background-color: gray;"> 4</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_4">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 9</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja9_">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 14</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_14">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 19</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_19">
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td class="fw-bold" style="background-color: gray;"> 5</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_5">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 10</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_10">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 15</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_15">
+                            </td>
+                            <td class="fw-bold" style="background-color: gray;"> 20</td>
+                            <td>
+                                <input class="text-uppercase" type="text" name="caja_20">
+                            </td>
+                        </tr>
+
+
+
+                        
+
+
+
+                    </tbody>
+                </table>
+
+
+
+            </div>
+            <div class="text-start  fs-5 fw-semibold" style="margin-top: 20px;">
+                Observaciones:
+                <textarea class="form-control" id="observacion_higiene" rows="3"></textarea>
+
+
+            </div>
+
+
+
+
+        </div>
+    </div>
+</div>
