20e68b18eeb296d258f40a832fb1855650d28b00
PC PORTATIL <fernando95f4@gmail.com>
2023-02-01 21:01:52 -0500

    Actualizacion de listado de reportes para la consulta

--- mvc/modelo/Grupo 3/modelo_reportes_garita.php
+++ mvc/modelo/Grupo 3/modelo_reportes_garita.php
@@ -6,7 +6,7 @@ $conexion = conexionBd();
 
 $cantidad_por_pagina = 5;
 
-if ((empty($_POST["button-busqueda"]) && empty($_SESSION["sesion_busqueda"])) || empty($_GET["busqueda"])) {
+if ((empty($_POST["button-busqueda-reportes"]) && empty($_SESSION["sesion_busqueda_reportes"])) || empty($_GET["busqueda"])) {
     $sql_total = $conexion->query("SELECT COUNT(*) AS total FROM vista_registro_llegada;");
     $total = $sql_total->fetch_assoc()['total'];
     $num_pags = ceil($total / $cantidad_por_pagina);
@@ -15,18 +15,18 @@ if ((empty($_POST["button-busqueda"]) && empty($_SESSION["sesion_busqueda"])) ||
     $inicio = ($pagina - 1) * $cantidad_por_pagina;
 
     $sql = $conexion->query("SELECT * FROM vista_registro_llegada LIMIT $inicio, $cantidad_por_pagina;");
-    unset($_SESSION["sesion_busqueda"]);
+    unset($_SESSION["sesion_busqueda_reportes"]);
 } else {
     if (!empty($_POST["seleccionBusquedaGarita"]) && !empty($_POST["busqueda_garita"])) {
         $filtro_busqueda = $_POST["seleccionBusquedaGarita"];
         $valor_busqueda = $_POST["busqueda_garita"];
-    } elseif (!empty($_SESSION["sesion_busqueda"])) {
+    } elseif (!empty($_SESSION["sesion_busqueda_reportes"])) {
         $filtro_busqueda = $_SESSION["sesion_filtro"];
-        $valor_busqueda = $_SESSION["sesion_busqueda"];
+        $valor_busqueda = $_SESSION["sesion_busqueda_reportes"];
     }
 
     $_SESSION["sesion_filtro"] = $filtro_busqueda;
-    $_SESSION["sesion_busqueda"] = $valor_busqueda;
+    $_SESSION["sesion_busqueda_reportes"] = $valor_busqueda;
 
     $sql_total = $conexion->query("SELECT COUNT(*) AS total FROM vista_registro_llegada WHERE $filtro_busqueda = '$valor_busqueda';");
     $total = $sql_total->fetch_assoc()['total'];
@@ -35,5 +35,5 @@ if ((empty($_POST["button-busqueda"]) && empty($_SESSION["sesion_busqueda"])) ||
     $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
     $inicio = ($pagina - 1) * $cantidad_por_pagina;
 
-    $sql = $conexion->query("SELECT * FROM vista_registro_llegada WHERE $filtro_busqueda = '$valor_busqueda' LIMIT $inicio, $cantidad_por_pagina;");
+    $sql = $conexion->query("SELECT * FROM vista_registro_llegada WHERE fecha_inspeccion >= '2023-01-25' && fecha_inspeccion <= '2023-01-28' LIMIT $inicio, $cantidad_por_pagina;");
 }
--- mvc/vista/vista_reportes_garita_g3.php
+++ mvc/vista/vista_reportes_garita_g3.php
@@ -11,13 +11,117 @@ include_once "./Grupo 3/menu_garita.php";
                 <a id="ico-home" href="./dashboard.php"><img src="./../../img/home_32x32-escritorio.png" alt="icono-home" /></a>
             </div>
             <div class="col-10 me-3 ms-3 mt-4">
-                <div id="titulo-escritorio-reportes">
+                <div id="titulo-escritorio-listado">
                     REPORTE DE DATOS LLEGADA
                 </div>
             </div>
             <!-- /Titulo o encabezado del escritorio -->
         </div>
         <div id="caja-separacion-escritorio" class="mx-auto"></div>
+
+        <?php
+        include_once("./../controlador/Grupo 3/eliminar_registro_garita.php");
+        include("./../modelo/Grupo 3/modelo_listado_garita.php");
+        ?>
+
+        <div class="d-flex justify-content-center align-items-center flex-row mt-4">
+            <form action="#" method="post">
+                <div class="input-group" style="width: 700px;">
+                    <div class="d-flex align-items-center me-1 fw-semibold">Desde:</div><input name="busqueda_reporte_inicio" type="date" class="form-control" value="">
+                    <div class="d-flex align-items-center me-1 ms-1 fw-semibold">Hasta:</div><input name="busqueda_reporte_final" type="date" class="form-control" value="">
+                    <button class="btn btn-outline-primary" type="submit" name="button-busqueda-reportes" value="buscando">Buscar registros</button>
+                </div>
+            </form>
+            <?php
+            ?>
+
+        </div>
+        <!-- /Barra de busqueda del listado llegada -->
+
+        <table class="table table-striped table-hover table-sm table-bordered border-dark align-middle mt-5 mx-auto">
+            <thead class="bg-primary bg-gradient bg-opacity-75">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">FECHA INSPECCION</th>
+                    <th scope="col">SEMANA</th>
+                    <th scope="col">HORA LLEGADA</th>
+                    <th scope="col">CONTENEDOR</th>
+                    <th scope="col">CHOFER</th>
+                    <th scope="col">ACOPIO</th>
+                    <th scope="col">TP CAJA</th>
+                    <th scope="col">TP CONTENEDOR</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <?php
+                // include_once("./../modelo/Grupo 3/modelo_listado_garita.php");
+                while ($datos = $sql->fetch_object()) {
+                ?>
+                    <tr>
+                        <td><?= $datos->id ?></td>
+                        <td><?= $datos->fecha_inspeccion ?></td>
+                        <td><?= $datos->semana ?></td>
+                        <td><?= $datos->hora_llegada ?></td>
+                        <td><?= $datos->contenedor ?></td>
+                        <td><?= $datos->chofer_nombres ?></td>
+                        <td><?= $datos->nombre_acopio ?></td>
+                        <td><?= $datos->tipo_de_caja ?></td>
+                        <td><?= $datos->tipo_de_contenedor ?></td>
+                    </tr>
+                <?php }
+                ?>
+
+            </tbody>
+        </table>
+
+        <?php
+        if (empty($_GET["busqueda"])) {
+        ?>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <?php if ($pagina != 1) { ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?pagina=<?= $pagina - 1 ?>">Previous</a>
+                        </li>
+                    <?php } ?>
+                    <?php for ($i = 1; $i <= $num_pags; $i++) { ?>
+                        <li class="page-item <?= $i == $pagina ? 'active' : '' ?>">
+                            <a class="page-link" href="?pagina=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                    <?php } ?>
+                    <?php if ($pagina != $num_pags) { ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?pagina=<?= $pagina + 1 ?>">Next</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </nav>
+        <?php
+        } else {
+        ?>
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <?php if ($pagina != 1) { ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?busqueda=1&pagina=<?= $pagina - 1 ?>">Previous</a>
+                        </li>
+                    <?php } ?>
+                    <?php for ($i = 1; $i <= $num_pags; $i++) { ?>
+                        <li class="page-item <?= $i == $pagina ? 'active' : '' ?>">
+                            <a class="page-link" href="?busqueda=1&pagina=<?= $i ?>"><?= $i ?></a>
+                        </li>
+                    <?php } ?>
+                    <?php if ($pagina != $num_pags) { ?>
+                        <li class="page-item">
+                            <a class="page-link" href="?busqueda=1&pagina=<?= $pagina + 1 ?>">Next</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </nav>
+        <?php
+        }
+        ?>
+
     </div>
 </div>
 <?php
