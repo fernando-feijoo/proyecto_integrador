5571fc6a2dc4e5b271d9089c913689b499179736
chuchei <chuchei5243@gmail.com>
2023-02-09 02:11:45 -0500

    guardado sellos

--- mvc/controlador/Grupo 3/registro_sellos.php
+++ mvc/controlador/Grupo 3/registro_sellos.php
@@ -3,7 +3,7 @@
 
 if (!empty($_POST["btn-guardar"]) || !empty($_POST["btn-actualizar"])) {
 	if (!empty($_POST["sello_interno1"])) {
-		$id_sellos=$_POST['numCont'];
+		$id_sellos=$_SESSION['numCont'];
 		$sellos_internos = $_POST["sello_interno1"].';';
 		$sellos_internos .= $_POST["sello_interno2"].';';
 		$sellos_internos .= $_POST["sello_interno3"];
@@ -17,20 +17,27 @@ if (!empty($_POST["btn-guardar"]) || !empty($_POST["btn-actualizar"])) {
 		$sellos_externos .=$_POST["sello_externo7"].';';
 		$sellos_externos .=$_POST["sello_externo8"].';';
 		$sellos_externos .=$_POST["sello_externo9"];
-		$num_conte=$_POST["numCont"];
-		$id_registro=$_POST["id_regis"];
+		$num_conte=$_SESSION["numCont"];
+		$id_registro=$_SESSION["id_regis"];
 
 
 		// Para visualizar en pantalla de trabajo, prueba.
 		$conexion = conexionBd();
 
-		$sql_consulta = "SELECT `InsertarDatosContExport` ($id_sellos,'$num_conte','$sellos_externos','$sellos_internos','$id_registro');";
+		$sql_consulta = "SELECT `InsertarDatosContExport` ($id_sellos,'$sellos_externos','$sellos_internos','$id_registro');";
 		
 		if (($conexion->query($sql_consulta) === TRUE || !empty($_POST["btn-guardar"])) || ($conexion->query($sql_consulta) === TRUE || !empty($_POST["btn-actualizar"]))) {
 			if (!empty($_POST["btn-guardar"])) {
 				echo "<div class='alert alert-success text-center' id='alertas' role='alert' style='width: 85%; margin: auto !important; margin-top: 1rem !important;'>
         ¡Datos guardados correctamente!
-    </div>";
+		
+		
+
+    </div >" ;
+
+	echo"<script> window.location.href = ' vista_contenedores_g3.php'</script>";
+
+
 			} else {
 				echo "<div class='alert alert-success text-center' id='alertas' role='alert' style='width: 85%; margin: auto !important; margin-top: 1rem !important;'>
         ¡Datos actualizados correctamente!
@@ -46,4 +53,6 @@ if (!empty($_POST["btn-guardar"]) || !empty($_POST["btn-actualizar"])) {
         ¡Registre todos los datos necesarios, verifique!
     </div>";
 	}
+
 }
+
