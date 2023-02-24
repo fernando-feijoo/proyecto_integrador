2fa5861fed9c739b1fb8835d87878216e926720b
sitrayas <43706671+sitrayas@users.noreply.github.com>
2023-02-24 16:49:13 -0500

    puto carmona

--- mvc/vista/vista_se_g1.php
+++ mvc/vista/vista_se_g1.php
@@ -1,4 +1,48 @@
 <div class="Container">
+
+
+<div class="abreviaturas_eva" style="text-align: left;" >
+  <div class="row row-cols-6">
+    <div class="col"><b>AB</b> Pulpa Blanda/crema</div>
+    <div class="col"><b>CT</b> Corte cuchillo</div>
+    <div class="col"><b>MF</b> Dedos Mutilados</div>
+    <div class="col"><b>SC</b> Rasguño </div>
+    <div class="col"><b>TF</b> Excaso de cuñas</div>
+    <div class="col"><b>UD</b> Decoloracion Interior</div>
+    <div class="col"><b>BM</b> Colapsis</div>
+    <div class="col"><b>FF</b> Dedos Mellizos</div> 
+    <div class="col"><b>NI</b> Cuello Roto</div>
+    <div class="col"><b>SD</b> Escaldadura</div>
+    <div class="col"><b>TH</b> Trips</div>
+    <div class="col"><b>BR</b> Daño de Pulpa</div>
+    <div class="col"><b>FL</b> Flores</div>
+    <div class="col"><b>NS</b> Exceso</div>
+    <div class="col"><b>SK</b> Speckling</div> 
+    <div class="col"><b>TS</b> Dedo Corto</div>
+    <div class="col"><b>CC</b> Cicatriz Crecimiento</div>
+    <div class="col"><b>LF</b> Cicatriz de Hoja</div>
+    <div class="col"><b>OG</b> Sobre grado</div>
+    <div class="col"><b>SM</b> Fumagina</div>
+    <div class="col"><b>YP</b> Punta Amarilla</div>
+    <div class="col"><b>CH</b> Chimera</div> 
+    <div class="col"><b>LG</b> Latex Gelatinoso</div>
+    <div class="col"><b>PD</b> Punto Jhonson</div>
+    <div class="col"><b>SR</b> Estropeo</div>
+    <div class="col"><b>UG</b> Bajo grado</div>
+    <div class="col"><b>Cl</b> Daño químico</div>
+    <div class="col"><b>LS</b> Latex Seco</div>
+    <div class="col"><b>PS</b> Daño de punta</div> 
+    <div class="col"><b>SRF</b> Quema cartón</div>
+    <div class="col"><b>WI</b> Cochinilla</div>
+    <div class="col"><b>CS</b> Costra Gusano</div>
+    <div class="col"><b>ML</b> Mál Formado</div>
+    <div class="col"><b>RR</b> Manchá Roja</div>
+    <div class="col"><b>TC</b> Moquillo</div>
+    <div class="col"><b>WP</b> Pedunculo marchito</div>
+  </div>
+</div>
+    <br> </br>
+ <!--   
 <div class="row">
     <div class="col-2 d-flex" style="background-color: aqua;">
     <div class="peso">
@@ -19,7 +63,30 @@
     </div>
     <div class="col-1" style="background-color: brown;">
     <h3>hi</h3>
-    </div>
-</div>
-    
+    </div> -->
+    <table width="982" height="76" border="1">
+	  <tbody>
+	    <tr>
+	      <td width="48" aligned="center" >Peso Neto Lb</td>
+	      <td width="50" aligned="center" >Cluster por caja</td>
+	      <td width="48" aligned="center">PH</td>
+	      <td width="455" align="center">Selección</td>
+	      <td width="198" align="center">Empaque</td>
+	      <td width="143" align="center">Total Defectos</td>
+        </tr>
+	    <tr>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+	      <td>&nbsp;</td>
+        </tr>
+  </tbody>
+</table>
+
+
+
+</div> 
+
 </div>
